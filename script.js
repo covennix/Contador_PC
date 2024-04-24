5c8478845caa17553615ace9ac20e416719d33a3
Nix <130068039+covennix@users.noreply.github.com>
2024-04-24 11:36:05 -0300

    2

--- script.js
+++ script.js
@@ -2,25 +2,37 @@ const inputTexto = document.querySelector("[input-texto]");
 const divContador = document.querySelector("[txt-contador]");
 const btnContar = document.querySelector("[btn-contar]");
 
-let textoDigitado = "";
+let modo = "caracteres";
 
-inputTexto.addEventListener("input", () => {
-    textoDigitado = inputTexto.value;
-});
-
+inputTexto.addEventListener("input", handleInputTexto);
 btnContar.addEventListener("click", handleBtnContar);
 
-function handleBtnContar() {
-    contarCaracteres(textoDigitado);
-    contarPalavras(textoDigitado);
+function handleInputTexto() {
+    if (modo === "palavras") {
+        contarPalavras();
+    }
 }
 
-function contarCaracteres(texto) {
+function handleBtnContar() {
+    if (modo === "caracteres") {
+        modo = "palavras";
+        btnContar.innerText = "Contar Caracteres";
+        contarPalavras();
+    } else {
+        modo = "caracteres";
+        btnContar.innerText = "Contar Palavras";
+        contarCaracteres();
+    }
+}
+
+function contarCaracteres() {
+    const texto = inputTexto.value;
     const numCaracteres = texto.length;
     divContador.innerText = numCaracteres + " caracteres";
 }
 
-function contarPalavras(texto) {
-    const numPalavras = texto.trim().split(/\s+/).filter(word => word !== "").length;
-    divContador.innerText += ", " + numPalavras + " palavras";
+function contarPalavras() {
+    const texto = inputTexto.value.trim();
+    const numPalavras = texto.split(/\s+/).filter(word => word !== "").length;
+    divContador.innerText = numPalavras + " palavras";
 }
